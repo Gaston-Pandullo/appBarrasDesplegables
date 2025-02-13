d825858dbc8bd5f7efaceb4bf9080212cd27f2a8
Gastón Pandullo <gpandullo3@gmail.com>
2025-02-13 15:06:14 -0300

    Primer listado

--- app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
+++ app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.barrasdesplegables.ui.theme.BarrasDesplegablesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,32 +24,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BarrasDesplegablesTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    nombreColumnas: List<String> = listOf("Mundo", "Compose")
+){
+    Column(modifier = modifier.padding(vertical = 4.dp)){
+        for(nombre in nombreColumnas){
+            Greeting(name = nombre)
+        }
+    }
+
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-Surface(color = MaterialTheme.colorScheme.primary) {
+Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp).fillMaxWidth()) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(26.dp)
     )
 }
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BarrasDesplegablesTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
