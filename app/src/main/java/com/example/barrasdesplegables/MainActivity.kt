579d51b4da61ce829727662e2ab2f8247d6d2944
Gastón Pandullo <gpandullo3@gmail.com>
2025-02-17 17:01:12 -0300

    Agregado de vista previa.

--- app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
+++ app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +17,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,6 +38,38 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun PantallaPrevia(modifier: Modifier = Modifier){
+    var mostrarPantallaPrevia by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bienvenido!"
+        )
+        ElevatedButton(
+            onClick = {},
+            modifier = modifier
+        ) {
+            Text(
+                text = "Continue"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 350, heightDp = 350)
+@Composable
+fun PreviewPantallaCarga(){
+    BarrasDesplegablesTheme{
+        PantallaPrevia()
+    }
+}
+
 
 @Composable
 fun MyApp(
