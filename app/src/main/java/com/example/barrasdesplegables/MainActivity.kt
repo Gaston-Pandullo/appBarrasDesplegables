67fcc28974d922d8fc2aaef4f5448fd29c081e6c
Gastón Pandullo <gpandullo3@gmail.com>
2025-02-18 16:59:25 -0300

    Agregado - Pantalla previa

--- app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
+++ app/src/main/java/com/example/barrasdesplegables/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +46,7 @@ class MainActivity : ComponentActivity() {
 fun MyApp(
     modifier: Modifier = Modifier
 ){
-    var mostrandoPantallaPrevia by remember { mutableStateOf(true) }
+    var mostrandoPantallaPrevia by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if(mostrandoPantallaPrevia){
@@ -78,10 +81,12 @@ fun PantallaPrevia(onContinueClicked: () -> Unit ,modifier: Modifier = Modifier 
 
 @Composable
 private fun Greetings(modifier: Modifier = Modifier,
-              nombreColumnas: List<String> = listOf("Mundo", "Compose")){
-    Column(modifier = modifier.padding(vertical = 4.dp)){
-        for(nombre in nombreColumnas){
-            Greeting(name = nombre)
+              nombreColumnas: List<String> = List(1000) {"$it"}){
+    LazyColumn(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        items(items = nombreColumnas){
+            nombreColumnas -> Greeting(name = nombreColumnas)
         }
     }
 }
@@ -90,7 +95,7 @@ private fun Greetings(modifier: Modifier = Modifier,
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     // Variable de estado del boton.
-    var expanded = remember { mutableStateOf(false) }
+    var expanded = rememberSaveable { mutableStateOf(false) }
 
     val extraPadding = if(expanded.value) 48.dp else 0.dp
 
